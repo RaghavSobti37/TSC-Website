dfb4f4de374d9cf1c02fc74012e3da8cfea3b636
RaghavSobti37 <raghavsobti37@gmail.com>
2025-12-29 19:41:33 +0530

    added demoday page

--- components/Header.js
+++ components/Header.js
@@ -1,13 +1,30 @@
 import Link from 'next/link';
 import DarkModeToggle from './DarkModeToggle';
 import Image from 'next/image';
+import { useTheme } from '../components/Theme';
 
 export default function Header() {
+  const { isDarkMode } = useTheme();
+  console.log('isDarkMode:', isDarkMode);
   return (
     <header className="fixed top-0 left-0 right-0 z-50 bg-transparent backdrop-blur-sm">
       <div className="container mx-auto px-6 py-4 flex justify-between items-center">
         <Link href="/">
-          <Image src="/assets/LogoArtboard 1.png" alt="The Shakti Collective Logo" width={150} height={100} />
+          {isDarkMode ? (
+            <Image
+              src="/assets/LogoArtboard 4 copy 6.png"
+              alt="The Shakti Collective Logo"
+              width={150}
+              height={100}
+            />
+          ) : (
+            <Image
+              src="/assets/LogoArtboard 1.png"
+              alt="The Shakti Collective Logo"
+              width={150}
+              height={100}
+            />
+          )}
         </Link>
         <DarkModeToggle />
       </div>
--- next.config.js
+++ next.config.js
@@ -1,7 +1,18 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-  swcMinify: true,
+    reactStrictMode: true,
+    async rewrites() {
+        return [
+          {
+            source: '/demoday',
+            destination: 'https://artist-spotlight-hub.vercel.app',
+          },
+          {
+            source: '/demoday/:path*',
+            destination: 'https://artist-spotlight-hub.vercel.app/:path*',
+          },
+        ]
+      },
 }
 
 module.exports = nextConfig
